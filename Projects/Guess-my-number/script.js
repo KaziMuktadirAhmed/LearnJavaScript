d1b83a7556cc249985d8ff660ada2de01e78528b
KaziMuktadirAhmed <kazisujoy@gmail.com>
2022-12-29 15:35:27 +0600

    adds again functionality

--- Projects/Guess-my-number/script.js
+++ Projects/Guess-my-number/script.js
@@ -1,24 +1,36 @@
 "use strict";
 
-const secrate = Math.trunc(Math.random() * 20) + 1;
+let secrate = Math.trunc(Math.random() * 20) + 1;
 let score = 10;
+
 document.querySelector(".guess").value = 0;
 document.querySelector(".score").textContent = score;
 
 document.querySelector(".check").addEventListener("click", () => {
   const value = Number(document.querySelector(".guess").value);
+
+  // invalid input
   if (!value) {
     document.querySelector(".message").textContent = "⛔ Not a number";
-  } else if (value == secrate) {
-    document.querySelector(".message").textContent = "🎉 Correct";
-  } else if (value > secrate) {
+  }
+  // correct guess
+  else if (value == secrate) {
+    document.querySelector(".message").textContent = "🎉 Correct Guess !!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").textContent = secrate;
+    document.querySelector(".number").style.width = "30rem";
+  }
+  // when guess is too high
+  else if (value > secrate) {
     if (score > 0) {
       document.querySelector(".message").textContent = "📈 Too high";
       score--;
     } else {
       document.querySelector(".message").textContent = "💥 You lost the game";
     }
-  } else if (value < secrate) {
+  }
+  // when guess is too low
+  else if (value < secrate) {
     if (score > 0) {
       document.querySelector(".message").textContent = "📉 Too low";
       score--;
@@ -26,8 +38,20 @@ document.querySelector(".check").addEventListener("click", () => {
       document.querySelector(".message").textContent = "💥 You lost the game";
     }
   } else {
-    document.querySelector(".message").textContent = "Start guessing ......";
+    document.querySelector(".message").textContent = "Start guessing ...";
   }
   document.querySelector(".score").textContent = score;
   console.log(`current value ${value}`);
+});
+
+document.querySelector(".again").addEventListener("click", () => {
+  score = 10;
+  secrate = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing ...";
+  document.querySelector(".guess").value = 0;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
