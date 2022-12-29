73b24e3200cb11fd2e56cf3aa1dffd04b28c6c98
KaziMuktadirAhmed <kazisujoy@gmail.com>
2022-12-29 18:58:21 +0600

    adds palyer switch partially

--- Projects/Pig-game/script.js
+++ Projects/Pig-game/script.js
@@ -1,20 +1,18 @@
 'use strict';
 
-let player_0_score = 0;
-let player_1_score = 0;
+const scores = [0, 0];
+let current_player = 0;
 let current_score = 0;
 
 const player_0_score_node = document.getElementById('score--0');
 const player_1_score_node = document.getElementById('score--1');
-const player_0_current_score_node = document.getElementById('current--0');
-const player_1_current_score_node = document.getElementById('current--1');
 const dice_node = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-player_0_score_node.textContent = player_0_score;
-player_1_score_node.textContent = player_1_score;
+player_0_score_node.textContent = scores[0];
+player_1_score_node.textContent = scores[1];
 dice_node.classList.add('hidden');
 
 btnRoll.addEventListener('click', event => {
@@ -27,6 +25,16 @@ btnRoll.addEventListener('click', event => {
 function evaluateDiceRoll(roll) {
   if (roll !== 1) {
     current_score += roll;
+    document.getElementById(`current--${current_player}`).textContent =
+      current_score;
   } else {
+    current_score = 0;
+    document.getElementById(`current--${current_player}`).textContent =
+      current_score;
+    switchPlayer();
   }
+}
+
+function switchPlayer() {
+  current_player = (current_player + 1) % 2;
 }
