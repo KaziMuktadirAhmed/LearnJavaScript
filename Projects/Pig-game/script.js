867ca0d0ee1d4837c5cddb62dad14d90024aedf9
KaziMuktadirAhmed <kazisujoy@gmail.com>
2022-12-29 18:15:49 +0600

    initiate pig game UI

--- Projects/Pig-game/script.js
+++ Projects/Pig-game/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const player_0_score_node = document.getElementById('score--0');
+const player_1_score_node = document.getElementById('score--1');
+const dice_node = document.querySelector('.dice');
+
+player_0_score_node.textContent = 0;
+player_1_score_node.textContent = 0;
+dice_node.classList.add('hidden');
