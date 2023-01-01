de25f7bccf67d3feb47b25de396403eaddce1081
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-01 13:14:21 +0600

    adds initialization function

--- Projects/Pig-game/script.js
+++ Projects/Pig-game/script.js
@@ -1,22 +1,41 @@
 'use strict';
 
-const scores = [0, 0];
-let current_player = 0;
-let current_score = 0;
-let game = true;
+let scores, current_player, current_score, game;
 
 const player_0_node = document.querySelector('.player--0');
 const player_1_node = document.querySelector('.player--1');
 const player_0_score_node = document.getElementById('score--0');
 const player_1_score_node = document.getElementById('score--1');
+const player_0_current_score_node = document.getElementById(`current--0`);
+const player_1_current_score_node = document.getElementById(`current--1`);
+
 const dice_node = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-player_0_score_node.textContent = scores[0];
-player_1_score_node.textContent = scores[1];
-dice_node.classList.add('hidden');
+const init = () => {
+  // state variables
+  scores = [0, 0];
+  current_player = 0;
+  current_score = 0;
+  game = true;
+
+  // DOM nodes
+  player_0_score_node.textContent = scores[0];
+  player_1_score_node.textContent = scores[1];
+  player_0_current_score_node.textContent = 0;
+  player_1_current_score_node.textContent = 0;
+
+  dice_node.classList.add('hidden');
+
+  player_0_node.classList.remove(`player--winner`);
+  player_1_node.classList.remove(`player--winner`);
+  player_0_node.classList.add(`player--active`);
+  player_1_node.classList.remove(`player--active`);
+};
+
+init();
 
 btnRoll.addEventListener('click', event => {
   if (game) {
@@ -47,6 +66,8 @@ btnHold.addEventListener('click', event => {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
 
 function evaluateDiceRoll(roll) {
   if (roll !== 1) {
