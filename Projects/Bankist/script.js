1020a455bbd61614fc0fa0e9424f2be45c36d799
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-04 13:42:37 +0600

    add coding challange 2

--- Projects/Bankist/script.js
+++ Projects/Bankist/script.js
@@ -134,8 +134,12 @@ const calcAverageHumanAge = function (ages) {
   // 2.
   const adults = humanAges.filter(age => age >= 18);
   // 3.
-  const average = adults.reduce((sum, cur) => sum + cur, 0) / adults.length;
+  const average = adults.reduce(
+    (sum, cur, i, arr) => sum + cur / arr.length,
+    0
+  );
 
   console.log(average);
 };
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
