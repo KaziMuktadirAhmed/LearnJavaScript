2c1091119b4f78574fd9e2ee3ce71d9923890a4f
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-04 14:14:16 +0600

    adds summary calculation

--- Projects/Bankist/script.js
+++ Projects/Bankist/script.js
@@ -109,37 +109,22 @@ const calcDisplayBalance = function (movements) {
 };
 calcDisplayBalance(account1.movements);
 
-///////////////////////////////////////
-// Coding Challenge #2
+const calcDisplaySummary = function (movements) {
+  const income = movements
+    .filter(mov => mov >= 0)
+    .reduce((accum, cur) => accum + cur, 0);
+  labelSumIn.textContent = `${income}€`;
 
-/* 
-Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+  const outging = movements
+    .filter(mov => mov < 0)
+    .reduce((accum, cur) => accum + cur, 0);
+  labelSumOut.textContent = `${Math.abs(outging)}€`;
 
-Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
-
-1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
-2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
-3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
-4. Run the function for both test datasets
-
-TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
-
-GOOD LUCK 😀
-*/
-
-const calcAverageHumanAge = function (ages) {
-  // 1.
-  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-  // 2.
-  const adults = humanAges.filter(age => age >= 18);
-  // 3.
-  const average = adults.reduce(
-    (sum, cur, i, arr) => sum + cur / arr.length,
-    0
-  );
-
-  console.log(average);
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(dep => (dep * 1.2) / 100)
+    .filter(inter => inter >= 1)
+    .reduce((accum, cur) => accum + cur, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+calcDisplaySummary(movements);
