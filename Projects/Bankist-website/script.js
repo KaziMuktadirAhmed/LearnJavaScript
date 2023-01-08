d73d9a2876176a4d3c1d4b2afc57478eb4be0623
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-08 15:47:12 +0600

    adds event propagation practice code

--- Projects/Bankist-website/script.js
+++ Projects/Bankist-website/script.js
@@ -35,3 +35,29 @@ document.addEventListener('keydown', function (e) {
 btnScrollTo.addEventListener('click', function (event) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document
+  .querySelector('.nav__link')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    this.style.backgroundColor = randomColor();
+  });
+
+document
+  .querySelector('.nav__links')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    this.style.backgroundColor = randomColor();
+  });
+
+document.querySelector('.nav').addEventListener('click', function (event) {
+  event.preventDefault();
+  this.style.backgroundColor = randomColor();
+});
