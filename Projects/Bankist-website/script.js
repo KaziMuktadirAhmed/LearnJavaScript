37e2d1fd37721285afd53bddd53ab1d2b5a6d20a
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-08 17:31:46 +0600

    adds tabbed component

--- Projects/Bankist-website/script.js
+++ Projects/Bankist-website/script.js
@@ -51,3 +51,24 @@ document
       document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
     }
   });
+
+// Tabbed component (operations part)
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(tab =>
+    tab.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
