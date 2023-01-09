b361e3b3dc58a8d53234dc81fa7627b61badf07f
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-09 14:06:37 +0600

    adds lazy image loading functionality

--- Projects/Bankist-website/script.js
+++ Projects/Bankist-website/script.js
@@ -117,6 +117,7 @@ const revealSection = function (entries, observer) {
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
+
   observer.unobserve(entry.target);
 };
 
@@ -130,3 +131,24 @@ allSections.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function (event) {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imageObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imageObserver.observe(img));
