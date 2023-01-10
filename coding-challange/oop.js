bb6995836374442783fa3c9fe0092b0d77d08b26
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-10 18:11:16 +0600

    adds coding challange 2

--- coding-challange/oop.js
+++ coding-challange/oop.js
@@ -15,23 +15,48 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+///////////////////////////////////////
+// Coding Challenge #2
 
-Car.prototype.accelarate = function () {
-  this.speed += 10;
-  console.log("accelarate", this.speed);
-};
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
 
-Car.prototype.break = function () {
-  this.speed -= 10;
-  console.log("Break", this.speed);
-};
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelarate() {
+    this.speed += 10;
+    console.log("accelarate", this.speed);
+  }
+  break() {
+    this.speed -= 10;
+    console.log("Break", this.speed);
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
 
 const bmw = new Car("BMW", 300);
 const mercedise = new Car("Mercedise", 350);
+const ford = new Car("Ford", 250);
 
 bmw.accelarate();
 mercedise.break();
+ford.accelarate();
+console.log(ford.speedUS);
+ford.speedUS = 300;
+console.log(ford);
