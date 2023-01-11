2aa3e8833592174db440a44dfda89124e8eea381
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-11 13:45:06 +0600

    adds google map style map tiles in leaflet

--- MAPTY/script.js
+++ MAPTY/script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using the geolocation API
+console.log('ok');
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    // Leaflet template code
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    return true;
+  },
+  function () {
+    alert('Could not get location');
+    return true;
+  }
+);
