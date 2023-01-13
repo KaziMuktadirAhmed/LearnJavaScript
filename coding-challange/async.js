d9347bcb78c2bb6c4f90ec9f31d92072603a3564
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-14 02:49:44 +0600

    adds XML api response practice

--- coding-challange/async.js
+++ coding-challange/async.js
@@ -9,5 +9,23 @@ request.open("GET", `https://restcountries.com/v3.1/name/bangladesh`);
 request.send();
 
 request.addEventListener("load", function () {
-  console.log(this.responseText);
+  //   console.log(this.responseText);
+  const [data] = JSON.parse(this.responseText);
+  console.dir(data);
+
+  const html = `<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 100000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.ben}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.BDT.name}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
 });
