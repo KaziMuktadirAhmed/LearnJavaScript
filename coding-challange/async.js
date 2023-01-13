b5a995a01f9183f8713d317e43f02fe8e8104b04
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-14 02:07:14 +0600

    adds html and css template file

--- coding-challange/async.js
+++ coding-challange/async.js
@@ -2,3 +2,12 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+
+const request = new XMLHttpRequest();
+const name = "Bangladesh";
+request.open("GET", `https://restcountries.com/v3.1/name/bangladesh`);
+request.send();
+
+request.addEventListener("load", function () {
+  console.log(this.responseText);
+});
