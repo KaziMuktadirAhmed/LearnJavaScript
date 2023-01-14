a7c963df7e592c8a8fef8fb318cc16ccd2abe262
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-14 14:28:51 +0600

    wraps XML req into function call

--- coding-challange/async.js
+++ coding-challange/async.js
@@ -3,29 +3,35 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-const request = new XMLHttpRequest();
-const name = "Bangladesh";
-request.open("GET", `https://restcountries.com/v3.1/name/bangladesh`);
-request.send();
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  const name = "Bangladesh";
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-request.addEventListener("load", function () {
-  //   console.log(this.responseText);
-  const [data] = JSON.parse(this.responseText);
-  console.dir(data);
+  request.addEventListener("load", function () {
+    //   console.log(this.responseText);
+    // console.log(this);
+    const [data] = JSON.parse(this.responseText);
+    console.dir(data);
 
-  const html = `<article class="country">
+    const html = `<article class="country">
   <img class="country__img" src="${data.flags.png}" />
   <div class="country__data">
-    <h3 class="country__name">${data.name.official}</h3>
+    <h3 class="country__name">${data.name}</h3>
     <h4 class="country__region">${data.region}</h4>
     <p class="country__row"><span>👫</span>${(
       +data.population / 100000
     ).toFixed(1)}</p>
-    <p class="country__row"><span>🗣️</span>${data.languages.ben}</p>
-    <p class="country__row"><span>💰</span>${data.currencies.BDT.name}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
   </div>
 </article>`;
 
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData("UAE");
+getCountryData("Bangladesh");
+getCountryData("USA");
