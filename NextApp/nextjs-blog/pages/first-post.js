083f1b8f1ec53bc6f7a8ff99b2345c3a0708e1bd
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-23 02:16:20 +0600

    adds dynamic routing

--- NextApp/nextjs-blog/pages/first-post.js
+++ NextApp/nextjs-blog/pages/first-post.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/Link";
-import Layout from "../../components/layout";
+import Layout from "../components/layout";
 import React from "react";
 
 export default function FirstPost() {
--- NextApp/nextjs-blog/pages/index.js
+++ NextApp/nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
 import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
@@ -30,17 +31,17 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <Link href="/posts/first-post">This is a link</Link>
+      <Link href="/first-post">This is a link</Link>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
