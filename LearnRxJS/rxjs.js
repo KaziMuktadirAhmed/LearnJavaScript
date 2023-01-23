276d9ef8f100ec21fdda4d462f3c8b559098084e
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-23 18:21:39 +0600

    adds observable demo

--- LearnRxJS/rxjs.js
+++ LearnRxJS/rxjs.js
@@ -6,11 +6,19 @@ function print(value) {
   document.body.appendChild(element);
 }
 
-let nums = [3, 5, 3, 3, 3, 2, 2, 242, 3];
-let evenNums$ = Rx.from(nums);
+let nums = [2, 2, 4, 6, 8];
+let observableNums$ = Rx.from(nums);
+let evenNums$ = Rx.Observable.create((subscriber) => {
+  for (let currentNum of nums) {
+    if (currentNum % 2 === 0) subscriber.next(currentNum);
+    else subscriber.error(`Value is not even`);
+  }
+  subscriber.complete("Done");
+});
 let observer = {
   next: (value) => print(`value from observable: ${value}`),
   error: (error) => console.error(error),
   complete: () => print(`All Done`),
 };
+observableNums$.subscribe(observer);
 evenNums$.subscribe(observer);
